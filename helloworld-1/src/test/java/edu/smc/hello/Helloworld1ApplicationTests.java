7b41c59a3135af0e862f6b484566a66c330a20bc
BikramGyawali <gyawalibikram0@gmail.com>
2025-04-25 07:28:01 +0545

    MAke new folder for helloworld-1

---
+++ helloworld-1/src/test/java/edu/smc/hello/Helloworld1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.smc.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helloworld1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
