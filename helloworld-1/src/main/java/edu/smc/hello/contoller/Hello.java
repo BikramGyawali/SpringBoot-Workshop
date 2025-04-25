6d2ed49f8c44a65e498e3a8ef80bfbb75333ef14
BikramGyawali <gyawalibikram0@gmail.com>
2025-04-25 07:44:16 +0545

    handle get api

--- helloworld-1/src/main/java/edu/smc/hello/contoller/Hello.java
+++ helloworld-1/src/main/java/edu/smc/hello/contoller/Hello.java
@@ -1,5 +1,13 @@
 package edu.smc.hello.contoller;
 
-public class Hello {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Hello {
+	
+	@GetMapping("/hello")
+ public String testApi() {
+	 return "Hello everyone";
+ }
 }
