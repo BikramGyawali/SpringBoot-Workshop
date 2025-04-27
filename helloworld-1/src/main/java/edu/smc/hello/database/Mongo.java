c39523db019fce8319b6f13ce85d408e6be7bbaa
BikramGyawali <gyawalibikram0@gmail.com>
2025-04-27 09:40:36 +0545

    Added mogo

--- helloworld-1/src/main/java/edu/smc/hello/database/Mongo.java
+++ helloworld-1/src/main/java/edu/smc/hello/database/Mongo.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Mongo {
  public String fetchData(boolean lazyloading) {
-	 return "this is your data";
+	 return "this is your  mongo data";
  }
 }
