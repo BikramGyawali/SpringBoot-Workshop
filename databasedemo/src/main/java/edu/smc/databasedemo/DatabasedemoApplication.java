3b4677a67ac3e35f10e0e6137c0b7e72f33d857c
BikramGyawali <gyawalibikram0@gmail.com>
2025-05-02 08:00:00 +0545

    Added new project for database demo

---
+++ databasedemo/src/main/java/edu/smc/databasedemo/DatabasedemoApplication.java
@@ -0,0 +1,13 @@
+package edu.smc.databasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasedemoApplication.class, args);
+	}
+
+}
